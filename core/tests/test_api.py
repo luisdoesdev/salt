0547676022f347b152390ad3f9a72cdabff75766
letorruella <letorruella@gmail.com>
2023-10-01 11:25:38 -0400

    test: rounte handler, observability

--- core/tests/test_api.py
+++ core/tests/test_api.py
@@ -1,5 +1,7 @@
 import unittest
 from core.api import API
+import logging
+
 
 class TestAPI(unittest.TestCase):
     def setUp(self):
@@ -12,6 +14,30 @@ class TestAPI(unittest.TestCase):
         expected_response_text = "Hello, my friend with user agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
         response = self.api.handle_request(MockRequest())
         self.assertEqual(response.text, expected_response_text)
+    
+    def test_route_decorator(self):
+        '''
+        Test the functionality of the route decorator by adding routes to 
+        the API instance and printing the current routes.
+        
+        It's interactive as well, so you can play around with it.
+        
+        '''
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.debug('Starting test_route_decorator')
+        def add_route(route_names):
+            if len(route_names) == 0:
+                return
+            for route_name in route_names:
+                @self.api.route(f'/{route_name}')
+                def route_function(request, response):
+                    response.text = {route_name}
+                    logging.info(f'Response text set to {response.text}')
+
+        route_names = ['home','about'] # feel free to add remove route names
+        add_route(route_names)
+        logging.debug(f'This are the current routes: {self.api.routes.keys()}')
+   
 
 if __name__ == '__main__':
     unittest.main()
