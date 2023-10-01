dbcec7c66cb4f3b832f54de2e7f6440f1d9947fb
letorruella <letorruella@gmail.com>
2023-10-01 11:24:11 -0400

    feat:route handler on API class

--- core/api.py
+++ core/api.py
@@ -5,12 +5,22 @@ from webob import Request, Response
 
 
 class API:
+    def __init__(self) -> None:
+        self.routes = {}
+    
     def __call__(self, environ, start_response):
         request = Request(environ)
 
         response = self.handle_request(request)
 
         return response(environ, start_response)
+
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+        
+        return wrapper
 
     def handle_request(self, request):
         user_agent = request.environ.get('HTTP_USER_AGENT', 'No User Agent Found')
